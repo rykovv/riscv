691a499632a8fbc9eba2f0f0fbc7cc4f0624dc9d
Vladislav Rykov <vrykov@csus.edu>
2021-11-09 22:54:12 -0800

    alu implemented

--- alu.v
+++ alu.v
@@ -3,10 +3,25 @@ module alu #(
 ) (
   input [WORDSIZE-1:0] A, B,
   input [3:0] CTL,
-  output z,
-  output [WORDSIZE-1:0] R
+  output Z,
+  output reg [WORDSIZE-1:0] R
 );
+  localparam
+    AND = 4'b0000,
+    OR  = 4'b0001,
+    ADD = 4'b0010,
+    SUB = 4'b0110;
+    
+  always @(*) 
+  begin
+    case (CTL):
+      AND: R = A & B;
+      OR : R = A | B;
+      ADD: R = A + B;
+      SUB: R = A - B;
+    endcase
+  end
 
-
+  assign Z = ~(|R);
 
 endmodule
